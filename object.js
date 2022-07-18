efd30a2c71330435a378a795402b6dff49bdbb84
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-18 15:26:13 +0300

    add js which hides or shows the form

--- object.js
+++ object.js
@@ -1,27 +1,29 @@
-let myLibrary=[];
-let container=document.querySelector(".container")
-function Book(title, author, pages, read){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
+let myLibrary = [];
+let container = document.querySelector(".container")
+let formButton = document.querySelector(".formButton")
+const formContainer = document.querySelector('.formContainer')
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
 }
-function addBookToLibrary(title, author, pages, read){
-    let newBook=new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
-function displayBooks(){
-    for (book of myLibrary){
-        const bookCard= document.createElement('div')
-        const bookName= document.createElement('div')
-        const bookAuthor= document.createElement('div')
-        const bookPages= document.createElement('div')
-        const bookRead=document.createElement('div')
-        bookName.textContent=book.title;
-        bookAuthor.textContent=book.author;
-        bookPages.textContent=book.pages;
-        bookRead.textContent=book.read;
+function displayBooks() {
+    for (book of myLibrary) {
+        const bookCard = document.createElement('div')
+        const bookName = document.createElement('div')
+        const bookAuthor = document.createElement('div')
+        const bookPages = document.createElement('div')
+        const bookRead = document.createElement('div')
+        bookName.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages;
+        bookRead.textContent = book.read;
         bookCard.classList.add("bookCard")
         container.appendChild(bookCard)
         bookCard.appendChild(bookName)
@@ -33,6 +35,16 @@ function displayBooks(){
 }
 /*const book1=new Book("house of leaves", "Dan", "700", "yes")
 console.log(book1)*/
+formButton.addEventListener('click', () => {
+    if (formContainer.hidden == false) {
+        formContainer.hidden = true;
+    }
+    else {
+        formContainer.hidden = false;
+    }
+
+}
+)
 addBookToLibrary("house of leaves", "Dan", "700", "yes")
 addBookToLibrary("house of leaves2", "Dan2", "7002", "yes2")
 addBookToLibrary("house of leaves3", "Dan3", "7003", "yes3")
